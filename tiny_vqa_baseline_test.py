809c99a76405ae8eb8c768916992d2b6a67abd15
fenghan0430 <fenghan0430@outlook.com>
2024-04-26 15:20:54 +0800

    调整学习率，分割验证集

--- tiny_vqa_baseline_test.py
+++ tiny_vqa_baseline_test.py
@@ -16,7 +16,10 @@ set_gpu_memory_mode()
 
 if __name__ == "__main__":
     dataset_dir = "floodnet_dataset"
-    batch_size = 32    
+    batch_size = 85
+    custom_learning_rate = 0.000001 #学习率
+    num_epochs = 50
+    buffer_size  = 4000   
     
     _data = vqa_data(dataset_dir)
     images, questions, answers, image_labels = _data.load_data()
@@ -27,15 +30,24 @@ if __name__ == "__main__":
         """用于解包元组并提供图像和问题"""
         return (image, question), (answer, label)
     dataset = dataset.map(process_data)
-    dataset = dataset.batch(batch_size)
+    dataset = dataset.shuffle(buffer_size=buffer_size).repeat(count=num_epochs) # GPT推荐加.repeat，但我不清楚有什么用
+    val_dataset = dataset.take(500)
+    dataset = dataset.skip(500)
+    dataset = dataset.batch(batch_size=batch_size)
+    val_dataset = val_dataset.batch(batch_size=batch_size)
     
     model_builder = vqa_model(num_answers)
     baseline_model = model_builder.baseline_vqa_model()
-    baseline_model.compile(optimizer='adam', loss=['categorical_crossentropy', 'categorical_crossentropy'], metrics=['accuracy'])
-    baseline_model.fit(dataset, epochs=10)
+    optimizer_adam = tf.keras.optimizers.Adam(learning_rate=custom_learning_rate)
+    baseline_model.compile(
+        optimizer=optimizer_adam,
+        loss=['categorical_crossentropy', 'categorical_crossentropy'],
+        metrics=['accuracy']
+        )
+    baseline_model.fit(dataset, epochs=num_epochs, validation_data=val_dataset)
     
     current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    weights_save_path = f"baseline2-{current_time}.h5"
+    weights_save_path = f"baseline3-{current_time}.h5"
     baseline_model.save_weights(weights_save_path)
     print(f"Weights saved to {weights_save_path}")
     
